1cc8f8dec8cbae9ed35b52c207deeff53e1b85a5
me-ron <meronkiflayyy@gmail.com>
2025-08-14 11:22:03 +0300

    some some stuff

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -1,2 +1,21 @@
 package main
 
+import (
+	"log"
+
+	"backend/config"
+)
+
+func main() {
+	// Load environment variables
+	cfg := config.LoadConfig()
+
+	// Init services
+	db := config.InitDB(cfg)
+	redisClient := config.InitRedis(cfg)
+	cloud := config.InitCloudinary(cfg)
+
+	log.Println(db, redisClient, cloud)
+
+	// http
+}
